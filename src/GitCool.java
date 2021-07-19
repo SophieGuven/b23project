41f5fb0fa6b48d2076a92c9de83a2adcd22ff80b
SophieGuven <teslimesofi@gmail.com>
2021-07-19 16:43:17 -0400

    added github account

--- src/GitCool.java
+++ src/GitCool.java
@@ -4,4 +4,9 @@ public class GitCool {
     //b23 is special number
     //b23 is making some sense now
 
+    //we have added our GitHub account under intellj
+    //make sure to click apply and click on to save
+
+
+
 }
